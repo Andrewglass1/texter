69fbf6fbc78d2ed291490ec5da9f907e4ccda65b
andy <andy@andys-MacBook-Pro.local>
2012-11-06 08:44:57 -0500

    fix

--- app/models/station.rb
+++ app/models/station.rb
@@ -12,11 +12,11 @@ class Station < ActiveRecord::Base
   def response_train_times
   	response = ""
   	fetch_times[0..5].each do |train|
-  		dest     = train["DestinationCode"]
+  		dest     = train["DestinationName"]
   		line     = train["Line"]
   		arriving = train["Min"]
 
-  		response <<"#{line} to #{dest} arriving in #{arriving}    
+  		response <<"#{line} to #{dest} arriving in #{arriving}
   		"
   	end
   	response
