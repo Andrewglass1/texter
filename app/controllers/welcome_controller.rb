138ec6a1936f30770477d39846685ff798c37985
Andrew Glass <andy.glass@livingsocial.com>
2012-11-25 21:55:26 -0500

    add notice on instructions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,9 @@ class WelcomeController < ApplicationController
       if send_to.length == 10
         response = Response.create(to: send_to, body: Response.text_instructions)
         response.transmit
+        notice = "okay hang tight, instructions are on the way"
       end
-    redirect_to root_path
+    redirect_to root_path, :notice => notice || "sorry, I cant figure out how to text #{params['send_to']}"
   end
 end
 
