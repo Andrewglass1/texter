a07bc43e2f6bd1c9a277ed4c6afe5e297bc652a0
Andrew Glass <andrewglass1@gmail.com>
2014-10-12 18:54:13 -0400

    changed db yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130216144229) do
+ActiveRecord::Schema.define(:version => 20121124223710) do
 
   create_table "hot_keys", :force => true do |t|
     t.string   "phone_number"
@@ -27,14 +27,6 @@ ActiveRecord::Schema.define(:version => 20130216144229) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "station_id"
-  end
-
-  create_table "photos", :force => true do |t|
-    t.string   "image"
-    t.string   "caption"
-    t.boolean  "active"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "responses", :force => true do |t|
@@ -59,15 +51,6 @@ ActiveRecord::Schema.define(:version => 20130216144229) do
     t.string   "station_together2"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
-  end
-
-  create_table "videos", :force => true do |t|
-    t.string   "image"
-    t.string   "video_link"
-    t.string   "caption"
-    t.boolean  "active"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
